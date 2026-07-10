662dda48b8e7a91ead8e935d4a0c76e84925b9e9
myrellasilvinha <myrella0711@gmail.com>
2026-07-09 22:55:48 -0500

    Summary

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://pouovktlcfqzvvcqiymh.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_iR7O58uPtKQ4MxvEH2OV4Q_-MH91vHO";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
